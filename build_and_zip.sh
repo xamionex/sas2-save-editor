79f82999a799db5d2336305032055f6daac62832
Petar <57235791+xamionex@users.noreply.github.com>
2026-08-30 06:24:25 +0200

    Update build script

--- build_and_zip.sh
+++ build_and_zip.sh
@@ -5,27 +5,34 @@ PROJECT="sas2-save-editor"
 OUT_DIR="out"
 
 WIN_TARGET="x86_64-pc-windows-gnu"
-LINUX_TARGET="x86_64-unknown-linux-gnu.2.28"
 
 WIN_BIN="target/$WIN_TARGET/release/${PROJECT}.exe"
-LINUX_BIN="target/$LINUX_TARGET/release/${PROJECT}"
+LINUX_BIN="target/release/${PROJECT}"
 
 WIN_ZIP="$OUT_DIR/${PROJECT}-windows.zip"
 LINUX_ZIP="$OUT_DIR/${PROJECT}-linux.zip"
 
+# Windows needs zigbuild for the cross-compile, the Linux binary is a native build.
 cargo zigbuild --target "$WIN_TARGET" --release &
 PID_WIN=$!
 
-cargo zigbuild --target "$LINUX_TARGET" --release &
+cargo build --release &
 PID_LINUX=$!
 
 wait $PID_WIN
 wait $PID_LINUX
 
-mkdir -p "$OUT_DIR"
+strip "$LINUX_BIN"
+x86_64-w64-mingw32-strip "$WIN_BIN"
 
-zip -j -9 "$WIN_ZIP" "$WIN_BIN"
-zip -j -9 "$LINUX_ZIP" "$LINUX_BIN"
+mkdir -p "$OUT_DIR" staging/win staging/linux
+cp "$WIN_BIN" "staging/win/"
+cp "$LINUX_BIN" "staging/linux/"
+
+(cd staging/win && 7z a -tzip -mx=9 -mpass=15 "../../$WIN_ZIP" *)
+(cd staging/linux && 7z a -tzip -mx=9 -mpass=15 "../../$LINUX_ZIP" *)
+
+rm -rf staging
 
 echo "  - $WIN_ZIP"
 echo "  - $LINUX_ZIP"
